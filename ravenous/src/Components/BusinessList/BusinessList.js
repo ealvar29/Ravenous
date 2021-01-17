9dab8099b13bd9609437bea0b9c214f916aefeb6
Eduardo <edalvarez2015@gmail.com>
2021-01-17 17:42:46 -0600

    Part 2 of Ravenous project

--- ravenous/src/Components/BusinessList/BusinessList.js
+++ ravenous/src/Components/BusinessList/BusinessList.js
@@ -6,12 +6,11 @@ import Business from '../Business/Business'
     render () {
         return (
             <div className="BusinessList">
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
+                {
+                this.props.businesses.map((business) => {
+                  return  <Business business={business} />;
+                })
+                }
             </div>
         ); 
     }
